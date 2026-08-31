81c023ccad7a7fe44cf34a192e28581dfdb9e8c8
Aidan <69904509+Silly06@users.noreply.github.com>
2026-08-31 11:38:45 +1000

    Spike for a draft backend (#1)
    * gitignore
    
    * add features.md
    
    * Draft app

---
+++ src/SmartBanking.Service/SmartBanking.Service/SmartBanking.Transactions.Service/TransactionsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartBanking.Transactions.Service
+{
+	[Route("Transactions")]
+	public class TransactionsController : Controller
+	{
+		public TransactionsController()
+		{
+
+		}
+
+		[Route("Create")]
+		public IActionResult CreateTransaction()
+		{
+			return Ok();
+		}
+
+	}
+}
