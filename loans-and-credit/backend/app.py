8878882e721338f72fc4db2664d048474f27388e
DavidL138 <160093599+DavidL138@users.noreply.github.com>
2026-09-03 22:36:22 +1000

    Update app.py

--- loans-and-credit/backend/app.py
+++ loans-and-credit/backend/app.py
@@ -33,4 +33,4 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=5002, debug=False, threaded=True)
